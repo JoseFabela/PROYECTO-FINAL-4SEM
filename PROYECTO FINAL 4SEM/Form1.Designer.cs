cfd0d75ee06016d0063ed71b1285f347ae4c9e37
JoseFabela <jfabela522@gmail.com>
2024-06-17 15:21:06 -0600

    Agregar archivos de proyecto.

---
+++ PROYECTO FINAL 4SEM/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace PROYECTO_FINAL_4SEM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.btnSaveToDatabase = new System.Windows.Forms.Button();
+            this.btnLoadFromDatabase = new System.Windows.Forms.Button();
+            this.btnSaveToXML = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnSaveToCSV = new System.Windows.Forms.Button();
+            this.btnSaveToJson = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1168, 300);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnLoadFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoadFile.FlatAppearance.BorderSize = 0;
+            this.btnLoadFile.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoadFile.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnLoadFile.ForeColor = System.Drawing.Color.White;
+            this.btnLoadFile.Location = new System.Drawing.Point(0, 306);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(152, 33);
+            this.btnLoadFile.TabIndex = 1;
+            this.btnLoadFile.Text = "LoadFile";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // btnSaveToDatabase
+            // 
+            this.btnSaveToDatabase.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSaveToDatabase.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveToDatabase.FlatAppearance.BorderSize = 0;
+            this.btnSaveToDatabase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveToDatabase.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSaveToDatabase.ForeColor = System.Drawing.Color.White;
+            this.btnSaveToDatabase.Location = new System.Drawing.Point(731, 306);
+            this.btnSaveToDatabase.Name = "btnSaveToDatabase";
+            this.btnSaveToDatabase.Size = new System.Drawing.Size(152, 37);
+            this.btnSaveToDatabase.TabIndex = 2;
+            this.btnSaveToDatabase.Text = "SaveToDatabase";
+            this.btnSaveToDatabase.UseVisualStyleBackColor = true;
+            this.btnSaveToDatabase.Click += new System.EventHandler(this.btnSaveToDatabase_Click);
+            // 
+            // btnLoadFromDatabase
+            // 
+            this.btnLoadFromDatabase.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnLoadFromDatabase.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLoadFromDatabase.FlatAppearance.BorderSize = 0;
+            this.btnLoadFromDatabase.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLoadFromDatabase.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnLoadFromDatabase.ForeColor = System.Drawing.Color.White;
+            this.btnLoadFromDatabase.Location = new System.Drawing.Point(551, 306);
+            this.btnLoadFromDatabase.Name = "btnLoadFromDatabase";
+            this.btnLoadFromDatabase.Size = new System.Drawing.Size(152, 37);
+            this.btnLoadFromDatabase.TabIndex = 3;
+            this.btnLoadFromDatabase.Text = "LoadFromDatabase";
+            this.btnLoadFromDatabase.UseVisualStyleBackColor = false;
+            this.btnLoadFromDatabase.Click += new System.EventHandler(this.btnLoadFromDatabase_Click);
+            // 
+            // btnSaveToXML
+            // 
+            this.btnSaveToXML.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSaveToXML.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveToXML.FlatAppearance.BorderSize = 0;
+            this.btnSaveToXML.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveToXML.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSaveToXML.ForeColor = System.Drawing.Color.White;
+            this.btnSaveToXML.Location = new System.Drawing.Point(961, 391);
+            this.btnSaveToXML.Name = "btnSaveToXML";
+            this.btnSaveToXML.Size = new System.Drawing.Size(207, 88);
+            this.btnSaveToXML.TabIndex = 4;
+            this.btnSaveToXML.Text = "SAVETOXML";
+            this.btnSaveToXML.UseVisualStyleBackColor = false;
+            this.btnSaveToXML.Click += new System.EventHandler(this.btnSaveToXML_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(158, 304);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 37);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add File";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSaveToCSV
+            // 
+            this.btnSaveToCSV.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSaveToCSV.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveToCSV.FlatAppearance.BorderSize = 0;
+            this.btnSaveToCSV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveToCSV.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSaveToCSV.ForeColor = System.Drawing.Color.White;
+            this.btnSaveToCSV.Location = new System.Drawing.Point(961, 478);
+            this.btnSaveToCSV.Name = "btnSaveToCSV";
+            this.btnSaveToCSV.Size = new System.Drawing.Size(207, 88);
+            this.btnSaveToCSV.TabIndex = 7;
+            this.btnSaveToCSV.Text = "SAVETOCSV";
+            this.btnSaveToCSV.UseVisualStyleBackColor = false;
+            this.btnSaveToCSV.Click += new System.EventHandler(this.btnSaveToCSV_Click);
+            // 
+            // btnSaveToJson
+            // 
+            this.btnSaveToJson.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnSaveToJson.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveToJson.FlatAppearance.BorderSize = 0;
+            this.btnSaveToJson.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSaveToJson.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnSaveToJson.ForeColor = System.Drawing.Color.White;
+            this.btnSaveToJson.Location = new System.Drawing.Point(961, 306);
+            this.btnSaveToJson.Name = "btnSaveToJson";
+            this.btnSaveToJson.Size = new System.Drawing.Size(207, 88);
+            this.btnSaveToJson.TabIndex = 8;
+            this.btnSaveToJson.Text = "SAVETOJSON";
+            this.btnSaveToJson.UseVisualStyleBackColor = false;
+            this.btnSaveToJson.Click += new System.EventHandler(this.btnSaveToJson_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 558);
+            this.Controls.Add(this.btnSaveToJson);
+            this.Controls.Add(this.btnSaveToCSV);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnSaveToXML);
+            this.Controls.Add(this.btnLoadFromDatabase);
+            this.Controls.Add(this.btnSaveToDatabase);
+            this.Controls.Add(this.btnLoadFile);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "ADD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.Button btnSaveToDatabase;
+        private System.Windows.Forms.Button btnLoadFromDatabase;
+        private System.Windows.Forms.Button btnSaveToXML;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSaveToCSV;
+        private System.Windows.Forms.Button btnSaveToJson;
+    }
+}
+
---
+++ PROYECTO FINAL 4SEM/Form1.cs
@@ -0,0 +1,365 @@
+﻿using System;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace PROYECTO_FINAL_4SEM
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection sqlConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDatabaseConnection();
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = new DataGridViewCellStyle
+            {
+                BackColor = Color.LightSteelBlue,
+                ForeColor = Color.Black,
+                Font = new Font("Segoe UI", 8.25F, FontStyle.Bold),
+                Alignment = DataGridViewContentAlignment.MiddleCenter
+            };
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = new DataGridViewCellStyle
+            {
+                BackColor = Color.WhiteSmoke
+            };
+            // Form Design Customization
+            this.BackColor = Color.LightGray; // Set background color
+            this.ForeColor = Color.Black; // Set text color
+            this.FormBorderStyle = FormBorderStyle.FixedSingle; // Set border style
+            this.Padding = new Padding(10); // Set padding around controls
+
+            // Layout Customization
+            this.ClientSize = new Size(800, 600); // Set form size
+            this.StartPosition = FormStartPosition.CenterScreen; // Center form on screen
+            this.AutoScaleMode = AutoScaleMode.Font; // Adjust font size based on screen resolution
+
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            // Background Design Customization
+            using (LinearGradientBrush brush = new LinearGradientBrush(this.ClientRectangle, Color.White, Color.LightBlue, 45f))
+            {
+                e.Graphics.FillRectangle(brush, this.ClientRectangle);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void InitializeDatabaseConnection()
+        {
+            string connectionString = "Server=FABELA;Database=ProyectoFinal4Sem;Integrated Security=True;";
+            sqlConnection = new SqlConnection(connectionString);
+
+            // Ejemplo con autenticación integrada de Windows
+            // string connectionString = "Server=localhost;Database=MyDatabase;Integrated Security=True;";
+            // sqlConnection = new SqlConnection(connectionString);
+        }
+
+        private void LoadDataFromDatabase()
+        {
+            try
+            {
+                sqlConnection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM Employee", sqlConnection);
+                DataTable dataTable = new DataTable();
+                sqlDataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading data: " + ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void SaveDataToDatabase()
+        {
+            try
+            {
+                sqlConnection.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM Employee", sqlConnection);
+                SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(sqlDataAdapter);
+                DataTable dataTable = (DataTable)dataGridView1.DataSource;
+                sqlDataAdapter.Update(dataTable);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving data: " + ex.Message);
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        private void LoadCsv(string filePath)
+        {
+            try
+            {
+                DataTable dataTable = new DataTable();
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    string[] headers = sr.ReadLine().Split(',');
+                    foreach (string header in headers)
+                    {
+                        dataTable.Columns.Add(header);
+                    }
+                    while (!sr.EndOfStream)
+                    {
+                        string[] rows = sr.ReadLine().Split(',');
+                        DataRow dataRow = dataTable.NewRow();
+                        for (int i = 0; i < headers.Length; i++)
+                        {
+                            dataRow[i] = rows[i];
+                        }
+                        dataTable.Rows.Add(dataRow);
+                    }
+                }
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading CSV: " + ex.Message);
+            }
+        }
+
+        private void LoadXml(string filePath)
+        {
+            try
+            {
+                DataSet dataSet = new DataSet();
+                dataSet.ReadXml(filePath);
+                dataGridView1.DataSource = dataSet.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading XML: " + ex.Message);
+            }
+        }
+
+        private void LoadJson(string filePath)
+        {
+            try
+            {
+                string jsonData = File.ReadAllText(filePath);
+                DataTable dataTable = JsonConvert.DeserializeObject<DataTable>(jsonData);
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading JSON: " + ex.Message);
+            }
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml|JSON files (*.json)|*.json"
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string extension = Path.GetExtension(filePath).ToLower();
+                switch (extension)
+                {
+                    case ".csv":
+                        LoadCsv(filePath);
+                        break;
+                    case ".xml":
+                        LoadXml(filePath);
+                        break;
+                    case ".json":
+                        LoadJson(filePath);
+                        break;
+                    default:
+                        MessageBox.Show("Unsupported file format");
+                        break;
+                }
+            }
+        }
+
+        private void btnSaveToDatabase_Click(object sender, EventArgs e)
+        {
+            SaveDataToDatabase();
+
+        }
+
+        private void btnLoadFromDatabase_Click(object sender, EventArgs e)
+        {
+            LoadDataFromDatabase();
+        }
+        private void SaveToXml(string filePath)
+        {
+            try
+            {
+                DataTable dataTable = (DataTable)dataGridView1.DataSource;
+                if (dataTable == null || dataTable.Rows.Count == 0)
+                {
+                    MessageBox.Show("No data to save!");
+                    return;
+                }
+
+                DataSet dataSet = new DataSet();
+                dataSet.Tables.Add(dataTable.Copy());
+                dataSet.WriteXml(filePath);
+
+                MessageBox.Show("Data saved successfully to XML: " + filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving to XML: " + ex.Message);
+            }
+        }
+
+        private void SaveToCsv(string filePath)
+        {
+            try
+            {
+                DataTable dataTable = (DataTable)dataGridView1.DataSource;
+                if (dataTable == null || dataTable.Rows.Count == 0)
+                {
+                    MessageBox.Show("No data to save!");
+                    return;
+                }
+
+                StringBuilder csvData = new StringBuilder();
+
+                // Write header row
+                foreach (DataColumn column in dataTable.Columns)
+                {
+                    csvData.Append(column.ColumnName + ",");
+                }
+                csvData.Remove(csvData.Length - 1, 1); // Remove trailing comma
+                csvData.AppendLine();
+
+                // Write data rows
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    for (int i = 0; i < row.ItemArray.Length; i++)
+                    {
+                        string cellValue = row.ItemArray[i].ToString();
+                        // Escape special characters for CSV (optional)
+                        // cellValue = cellValue.Replace(",", "\",");
+                        csvData.Append(cellValue + ",");
+                    }
+                    csvData.Remove(csvData.Length - 1, 1); // Remove trailing comma
+                    csvData.AppendLine();
+                }
+
+                File.WriteAllText(filePath, csvData.ToString());
+
+                MessageBox.Show("Data saved successfully to CSV: " + filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving to CSV: " + ex.Message);
+            }
+        }
+
+        private void SaveToJson(string filePath)
+        {
+            try
+            {
+                DataTable dataTable = (DataTable)dataGridView1.DataSource;
+                if (dataTable == null || dataTable.Rows.Count == 0)
+                {
+                    MessageBox.Show("No data to save!");
+                    return;
+                }
+
+                List<Dictionary<string, object>> jsonData = new List<Dictionary<string, object>>();
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    Dictionary<string, object> rowData = new Dictionary<string, object>();
+                    for (int i = 0; i < row.ItemArray.Length; i++)
+                    {
+                        rowData.Add(dataTable.Columns[i].ColumnName, row.ItemArray[i]);
+                    }
+                    jsonData.Add(rowData);
+                }
+
+                string jsonString = JsonConvert.SerializeObject(jsonData, Formatting.Indented);
+                File.WriteAllText(filePath, jsonString);
+
+                MessageBox.Show("Data saved successfully to JSON: " + filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving to JSON: " + ex.Message);
+
+            }
+        }
+
+        private void btnSaveToJson_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON files (*.json)|*.json";
+            saveFileDialog.Title = "Save data to XML";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                SaveToJson(filePath);
+            }
+        }
+
+        private void btnSaveToXML_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "XML files (*.xml)|*.xml";
+            saveFileDialog.Title = "Save data to XML";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                SaveToXml(filePath);
+            }
+        }
+
+        private void btnSaveToCSV_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            saveFileDialog.Title = "Save data to CSV";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                SaveToCsv(filePath);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataTable dataTable = (DataTable)dataGridView1.DataSource;
+
+            // Create a new row with empty values
+            DataRow newRow = dataTable.NewRow();
+            for (int i = 0; i < dataTable.Columns.Count; i++)
+            {
+                newRow[i] = DBNull.Value; // Set default values as appropriate
+            }
+
+            // Add the new row to the DataTable
+            dataTable.Rows.Add(newRow);
+
+            // Update the DataGridView to reflect the changes
+            dataGridView1.Refresh();
+        }
+    }
+
+}
